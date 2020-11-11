4dc4e33ac8377b51f43bd0f6e85e3f5bacdd7ebc
Volodymyr Kornieiev <Volydemar2032@gmail.com>
2020-11-11 13:51:26 +0200

    es6 import

--- src/index.js
+++ src/index.js
@@ -1,103 +1,107 @@
 import './styles.css';
-import ref from './js/refs'
-import fetchApi from './js/apiService'
-import template from './templates/gallery-template.hbs'
-import modalTemplate from './templates/modal.hbs'
-import btnState from './js/btn-state'
-import {notFoundNotice, someError} from './js/notofications'
-import debounce from 'lodash.debounce'
+import ref from './js/refs';
+import fetchApi from './js/apiService';
+import template from './templates/gallery-template.hbs';
+import modalTemplate from './templates/modal.hbs';
+import btnState from './js/btn-state';
+import { notFoundNotice, someError } from './js/notofications';
+import debounce from 'lodash.debounce';
 // var debounce = require('lodash.debounce');
-import 'basiclightbox/dist/basicLightbox.min.css'
-// import basicLightbox from 'basiclightbox' // не работает !!!
-const basicLightbox = require('basiclightbox')
-import infiniteLoading from './js/infiniteScroll'
+import 'basiclightbox/dist/basicLightbox.min.css';
+import * as basicLightbox from 'basiclightbox';
+// const basicLightbox = require('basiclightbox')
+import infiniteLoading from './js/infiniteScroll';
 
-preventDefaultForm()
-searchImg()
+preventDefaultForm();
+searchImg();
 
 ref.loadMoreBtn.addEventListener('click', () => {
-    ref.loadMoreBtn.classList.add('is-hidden')
-    searchMore()
-})
+  ref.loadMoreBtn.classList.add('is-hidden');
+  searchMore();
+});
 
-ref.container.addEventListener('click', (event) => {
-    const originalImgSrc = event.target.dataset.fullsize
-    
-    if (event.target.nodeName === 'IMG') {
-        showModal()
+ref.container.addEventListener('click', event => {
+  const originalImgSrc = event.target.dataset.fullsize;
 
-        const modalImg = document.querySelector('.modal-img');
-        modalImg.src = originalImgSrc
-    }
-})
+  if (event.target.nodeName === 'IMG') {
+    showModal();
+
+    const modalImg = document.querySelector('.modal-img');
+    modalImg.src = originalImgSrc;
+  }
+});
 
 function searchImg() {
-    ref.input.addEventListener('input', debounce(() => {
-        const inputValue = ref.input.value;
+  ref.input.addEventListener(
+    'input',
+    debounce(() => {
+      const inputValue = ref.input.value;
 
-        createGallery(inputValue)
-    }, 1000));
+      createGallery(inputValue);
+    }, 1000),
+  );
 }
 
 function createGallery(searchName) {
-    ref.container.innerHTML = '';
+  ref.container.innerHTML = '';
 
-    fetchApi.page = 1;
-    fetchApi.search = searchName;
+  fetchApi.page = 1;
+  fetchApi.search = searchName;
 
-    fetchApi.fetchImg()
-        .then(data => {
-            const arrayLength = data.hits.length
+  fetchApi
+    .fetchImg()
+    .then(data => {
+      const arrayLength = data.hits.length;
 
-            if (arrayLength === 0){
-                notFoundNotice()
-                btnState.hide()
-            }
-            
-            if (arrayLength > 0){
-            createMarkup(data)
-            btnState.show()
-            }
+      if (arrayLength === 0) {
+        notFoundNotice();
+        btnState.hide();
+      }
 
-            if (arrayLength < 12){
-            btnState.hide()
-            }
-        })
-        .catch(error => {
-            console.log(error)
-            someError()
-        })
+      if (arrayLength > 0) {
+        createMarkup(data);
+        btnState.show();
+      }
+
+      if (arrayLength < 12) {
+        btnState.hide();
+      }
+    })
+    .catch(error => {
+      console.log(error);
+      someError();
+    });
 }
 
 function searchMore() {
-    fetchApi.pageIncrement()
-    btnState.loading()
+  fetchApi.pageIncrement();
+  btnState.loading();
 
-    fetchApi.fetchImg()
-        .then(data => {
-            createMarkup(data)
-            btnState.loaded()
-           if (data.hits.length === 12){
-            infiniteLoading(ref.container.lastElementChild, searchMore)
-           }
-            // window.scrollTo({
-            //     top: document.documentElement.offsetHeight,
-            //     behavior: 'smooth'
-            // })
-        })
-        .catch(error => console.log(error))
+  fetchApi
+    .fetchImg()
+    .then(data => {
+      createMarkup(data);
+      btnState.loaded();
+      if (data.hits.length === 12) {
+        infiniteLoading(ref.container.lastElementChild, searchMore);
+      }
+      // window.scrollTo({
+      //     top: document.documentElement.offsetHeight,
+      //     behavior: 'smooth'
+      // })
+    })
+    .catch(error => console.log(error));
 }
 
 function createMarkup(data) {
-    const markup = template(data.hits)
-    ref.container.insertAdjacentHTML('beforeend', markup)
+  const markup = template(data.hits);
+  ref.container.insertAdjacentHTML('beforeend', markup);
 }
 
 function preventDefaultForm() {
-    ref.form.addEventListener('submit', (e) => e.preventDefault())
+  ref.form.addEventListener('submit', e => e.preventDefault());
 }
 
-function showModal(){
-    return basicLightbox.create(modalTemplate()).show()
+function showModal() {
+  return basicLightbox.create(modalTemplate()).show();
 }
-
